7e62c1032fcd581150321f4af6c3f0a83e1c287e
tsgin <isabelo@thesulfurgroup.com>
2019-12-10 17:22:56 -0800

    Testing netlify forms.

--- packages/landing-gatsby/src/containers/SaasClassic/Newsletter/index.js
+++ packages/landing-gatsby/src/containers/SaasClassic/Newsletter/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import $ from 'jquery'
 import PropTypes from "prop-types";
 import Box from "reusecore/src/elements/Box";
 import Text from 'reusecore/src/elements/Text';
@@ -6,6 +7,7 @@ import Heading from "reusecore/src/elements/Heading";
 import Button from "reusecore/src/elements/Button";
 import Input from "reusecore/src/elements/Input";
 import Container from "common/src/components/UI/Container";
+
 
 import NewsletterWrapper, { ContactFormWrapper } from "./newsletter.style";
 
@@ -48,30 +50,49 @@ import NewsletterWrapper, { ContactFormWrapper } from "./newsletter.style";
 // };
 
 function Newsletter({ sectionWrapper, textArea, buttonArea, buttonStyle, title, description }) {
-  const [formData, setFormData] = useState({
-    email: ""
-  });
+  // const [formData, setFormData] = useState({
+  //   email: ""
+  // });
 
-  const validateEmail = email => {
-    var re = /\S+@\S+\.\S+/;
+  // const validateEmail = email => {
+  //   var re = /\S+@\S+\.\S+/;
 
-    return re.test(email);
-  };
+  //   return re.test(email);
+  // };
 
-  const updateFormData = e => {
-    setFormData({
-      ...formData,
-      email: e
-    });
-  };
+  // const updateFormData = e => {
+  //   setFormData({
+  //     ...formData,
+  //     email: e
+  //   });
+  // };
+
+  // const handleSubmit = e => {
+  //   if (!formData.email) {
+  //     alert("Email field is required.");
+  //   } else if (!validateEmail(formData.email)) {
+  //     alert("The Email address you entered is invalid.");
+  //   } else {
+  //   }
+  // };
 
   const handleSubmit = e => {
-    if (!formData.email) {
-      alert("Email field is required.");
-    } else if (!validateEmail(formData.email)) {
-      alert("The Email address you entered is invalid.");
-    } else {
-    }
+    e.preventDefault()
+
+    // $("#my-form").submit(function(e) {
+    //   e.preventDefault();
+    
+    //   var $form = $(this);
+    //   $.post($form.attr("action"), $form.serialize()).then(function() {
+    //     alert("Thank you!");
+    //   });
+    // });
+
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize())
+    .then(function() {
+      alert("Thank you for contacting us! We will get in touch soon.");
+    });
   };
 
   return (
@@ -83,12 +104,44 @@ function Newsletter({ sectionWrapper, textArea, buttonArea, buttonStyle, title, 
             <Text content="Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolore" {...description} />
           </Box>
           <Box {...buttonArea}>
-            <ContactFormWrapper>
+            {/* <ContactFormWrapper>
               <Input inputType="email" label="Email address" iconPosition="right" isMaterial={true} className="email_input" arial-label="email" onChange={updateFormData} />
               <Button {...buttonStyle} title="GET ACCESS" onClick={handleSubmit} />
-            </ContactFormWrapper>
+            </ContactFormWrapper> */}
 
-            
+            <form id="form-1" name="contact" method="post" data-netlify="true" onSubmit={handleSubmit}>
+              <div className="field-1-1">
+                <label for="input-1-1">First name</label>
+                <input type="text" name="first_name" id="input-1-1" />
+              </div>
+              <div className="field-1-2">
+                <label for="input-1-2">Last name</label>
+                <input type="text" name="last_name" id="input-1-2" />
+              </div>
+              <div className="field-1-3">
+                <label for="input-1-3">Company</label>
+                <input type="text" name="company" id="input-1-3" />
+              </div>
+              <div className="field-1-4">
+                <label for="input-1-4">Email address</label>
+                <input type="email" name="email" id="input-1-4" />
+              </div>
+              <div className="field-1-5">
+                <label for="input-1-5">Phone number</label>
+                <input type="text" name="phone" id="input-1-5" />
+              </div>
+              <div className="field-1-6">
+                <label for="input-1-6">Website</label>
+                <input type="text" name="website" id="input-1-6" />
+              </div>
+              <div className="field-1-7">
+                <label for="input-1-7">Instagram URL</label>
+                <input type="text" name="instagram_url" id="input-1-7" />
+              </div>
+              <div>
+                <button type="submit">Submit</button> 
+              </div>
+            </form>
 
           </Box>
         </NewsletterWrapper>
