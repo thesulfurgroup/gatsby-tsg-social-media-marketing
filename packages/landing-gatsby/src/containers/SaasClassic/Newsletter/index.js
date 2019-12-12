bda1863c3994aa690e6c9c28da50a7bd1441d57e
tsgin <isabelo@thesulfurgroup.com>
2019-12-12 11:28:34 -0800

    Update subscribe form style.

--- packages/landing-gatsby/src/containers/SaasClassic/Newsletter/index.js
+++ packages/landing-gatsby/src/containers/SaasClassic/Newsletter/index.js
@@ -95,6 +95,46 @@ function Newsletter({ sectionWrapper, textArea, buttonArea, buttonStyle, title, 
           <Box {...textArea}>
             <Heading content="Subscribe our newsletter" {...title} />
             <Text content="Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolore" {...description} />
+
+            <form className="NetlifyForm" name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="NetlifyForm__fieldgroup">
+                <div className="NetlifyForm__field field-1-1">
+                  <label className="NetlifyForm__toplabel">Name</label>
+                  <input type="text" name="name" required placeholder="Your full name" />
+                </div>
+                <div className="NetlifyForm__field field-1-3">
+                  <label className="NetlifyForm__toplabel">Company</label>
+                  <input type="text" name="company" placeholder="Your company" />
+                </div>
+              </div>
+              <div className="NetlifyForm__fieldgroup">
+                <div className="NetlifyForm__field field-1-4">
+                  <label className="NetlifyForm__toplabel">Email address</label>
+                  <input type="email" name="email" required placeholder="Best email address to contact you" />
+                </div>
+                <div className="NetlifyForm__field field-1-5">
+                  <label className="NetlifyForm__toplabel">Phone number</label>
+                  <input type="text" name="phone" required placeholder="Best phone number to contact you" />
+                </div>
+              </div>
+              <div className="NetlifyForm__fieldgroup">
+                <div className="NetlifyForm__field field-1-6">
+                  <label className="NetlifyForm__toplabel">Website</label>
+                  <input type="url" name="website" placeholder="Do you have website?"/>
+                </div>
+                <div className="NetlifyForm__field field-1-7">
+                  <label className="NetlifyForm__toplabel">Instagram URL</label>
+                  <input type="url" name="instagram_url" placeholder="Do you have Instagram?" />
+                </div>
+              </div>
+              <div className="NetlifyForm__fieldgroup">
+                <div className="NetlifyForm__field">
+                  <Button {...buttonStyle} title="Submit" type="submit" />
+                </div>
+              </div>
+            </form>
           </Box>
           <Box {...buttonArea}>
             {/* <ContactFormWrapper>
@@ -102,42 +142,7 @@ function Newsletter({ sectionWrapper, textArea, buttonArea, buttonStyle, title, 
               <Button {...buttonStyle} title="GET ACCESS" />
             </ContactFormWrapper> */}
 
-            <form className="NetlifyForm" name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
-              <input type="hidden" name="bot-field" />
-              <input type="hidden" name="form-name" value="contact" />
-              <div className="NetlifyForm__field field-1-1">
-                <label className="NetlifyForm__toplabel">First name</label>
-                <input type="text" name="first_name" required placeholder="Your first name" />
-              </div>
-              <div className="NetlifyForm__field field-1-2">
-                <label className="NetlifyForm__toplabel">Last name</label>
-                <input type="text" name="last_name" required placeholder="Your last name" />
-              </div>
-              <div className="NetlifyForm__field field-1-3">
-                <label className="NetlifyForm__toplabel">Company</label>
-                <input type="text" name="company" placeholder="Your company" />
-              </div>
-              <div className="NetlifyForm__field field-1-4">
-                <label className="NetlifyForm__toplabel">Email address</label>
-                <input type="email" name="email" required placeholder="Best email to contact you" />
-              </div>
-              <div className="NetlifyForm__field field-1-5">
-                <label className="NetlifyForm__toplabel">Phone number</label>
-                <input type="text" name="phone" required placeholder="Your phone number" />
-              </div>
-              <div className="NetlifyForm__field field-1-6">
-                <label className="NetlifyForm__toplabel">Website</label>
-                <input type="url" name="website" placeholder="Do you have website?"/>
-              </div>
-              <div className="NetlifyForm__field field-1-7">
-                <label className="NetlifyForm__toplabel">Instagram URL</label>
-                <input type="url" name="instagram_url" placeholder="Do you have Instagram?" />
-              </div>
-              <div className="NetlifyForm__field">
-                <button type="submit" class="reusecore__button buttonstyle__ButtonStyle-leGlkG cYyrzA">Submit</button> 
-              </div>
-            </form>
-
+            
           </Box>
         </NewsletterWrapper>
       </Container>
@@ -157,8 +162,7 @@ Newsletter.propTypes = {
 Newsletter.defaultProps = {
   sectionWrapper: {},
   textArea: {
-    mb: ["40px", "40px", "40px", "0", "0"],
-    pr: ["0", "0", "0", "80px", "100px"]
+    width: "100%"
   },
   title: {
     fontSize: ["18px", "20px", "22px", "24px", "26px"],
@@ -174,7 +178,7 @@ Newsletter.defaultProps = {
     fontWeight: "400",
     color: "#fefefe",
     lineHeight: "1.7",
-    mb: 0,
+    mb: "30px",
     textAlign: ["center", "center", "center", "left", "left"]
   },
   buttonArea: {
--- packages/landing-gatsby/src/containers/SaasClassic/Newsletter/newsletter.style.js
+++ packages/landing-gatsby/src/containers/SaasClassic/Newsletter/newsletter.style.js
@@ -1,23 +1,22 @@
 import styled from 'styled-components';
 
 const NewsletterWrapper = styled.div`
+max-width: 800px;
+margin: 0 auto;
   position: relative;
   background: linear-gradient(
     -139deg,
     rgb(255, 65, 108) 0%,
     rgb(255, 75, 43) 100%
   );
-  padding: 44px 80px;
+  padding: 45px;
   border-radius: 10px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-top: -105px;
   @media (max-width: 1220px) {
-    padding: 35px 40px;
-  }
-  @media (max-width: 575px) {
-    padding: 35px 20px;
+    padding: 30px;
   }
   @media (max-width: 990px) {
     flex-wrap: wrap;
@@ -25,29 +24,49 @@ const NewsletterWrapper = styled.div`
   }
 
   .NetlifyForm {
-    min-width: 320px;
+    // max-width: 600px;
+    margin: 0 auto;
+
     input:not([type=radio]):not([type=checkbox]):not([type=submit]):not([type=button]):not([type=image]):not([type=file]) {
       border: 1px solid #eaeaea;
-      border-radius: 4px;
+      border-radius: 2px;
       color: #000000;
       font-size: 16px;
+      font-weight: 500;
       line-height: 1.5em;
       width: 100%;
-      padding: 8px;
+      padding: 8px 12px;
+      transition: all 450ms cubic-bezier(0.77, 0, 0.175, 1);
+
+      &::placeholder  {
+        color: transparent;
+        transition: all 450ms cubic-bezier(0.77, 0, 0.175, 1);
+      }
+
+      &:focus::placeholder {
+        color: #aaa;
+      }
+
+    }
+
+    &__fieldgroup {
+      display: flex;
+      margin-right: -8px;
+      margin-left: -8px;
     }
 
     &__field {
-      margin-bottom: 8px;
-
-      &:last-child {
-        margin-top: 16px;
-      }
+      flex: 1;
+      margin-bottom: 15px;
+      padding-right: 8px;
+      padding-left: 8px;
     }
 
     &__toplabel {
       color: #ffffff;
       display: inline-block;
       font-size: 14px;
+      font-weight: bold;
       margin-bottom: 4px;
     }
   }
